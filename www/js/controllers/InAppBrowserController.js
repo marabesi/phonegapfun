3cad80604eeb153212c7e0b5cd2e738a7c03f0e3
marabesi <matheus.marabesi@gmail.com>
2015-01-24 21:32:42 -0200

    Created the angular directive to use the inappbrowser plugin

--- www/js/controllers/InAppBrowserController.js
+++ www/js/controllers/InAppBrowserController.js
@@ -1,4 +1,15 @@
 'use strict';
 
+app.directive('inappbrowser', function ($window) {
+    return {
+        restrict: 'A',
+        link: function (scope) {
+            scope.search = function () {
+                $window.open(scope.location, '_blank');
+            }
+        }
+    }
+});
+
 app.controller('InAppBrowserController', ['$scope',
     function ($scope) {}]);
